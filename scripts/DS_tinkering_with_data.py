822e501a742460d58bffe22c2f7bcc42f95dcf3c
LoafBurger <olicheunglh@gmail.com>
2025-09-08 11:21:35 -0400

    remade orders to make it more clear

--- scripts/DS_tinkering_with_data.py
+++ scripts/DS_tinkering_with_data.py
@@ -1,30 +1,35 @@
 """
 Order of running scripts:
 - extract, filter, enrich, prepare, tinker
+    ├── DE_extract_starbucks.py
+    ├── DE_filter_reviews.py
+    ├── DE_enrich_reviews.py
+    ├── DE_prepare_analysis.py
+    └── DS_tinkering_with_data.py
 """
 
 import pandas as pd
 
-#Load the flattened CSV
+#load the flattened CSV
 csv_file = "data/processed/starbucks_reviews_flat.csv"
 df = pd.read_csv(csv_file)
 
-#See the first few rows
+#see the first few rows
 print("First 5 rows of the dataset:")
 print(df.head())
 
-#List all columns
+#list all columns
 print("\nColumns in the dataset:")
 print(df.columns.tolist())
 print(f"Number of Columns: {len(df.columns)}")
 
 
-#Example: get average review stars per city
+#get average review stars per city
 avg_stars_by_city = df.groupby("business_city")["stars"].mean().sort_values(ascending=False)
 print("\nAverage review stars by city:")
 print(avg_stars_by_city)
 
-#Example: see distribution of review stars
+#see distribution of review stars
 print("\nReview stars value counts:")
 print(df["stars"].value_counts())
 
