47662c9ff65d87f594955cc28547e2aa003dbe7c
Ethan <chan380@purdue.edu>
2025-10-10 05:36:31 -0400

    small comment and linting

--- scripts/DE_fix_missing.py
+++ scripts/DE_fix_missing.py
@@ -7,14 +7,21 @@ df = pd.read_csv(INPUT_CSV)
 
 
 # Fill missing values and drop irrelevant columns
-df['business_attributes.DriveThru'] = df['business_attributes.DriveThru'].fillna(False)
-df['business_attributes.OutdoorSeating'] = df['business_attributes.OutdoorSeating'].fillna(False)
-df['business_attributes.OutdoorSeating'] = df['business_attributes.OutdoorSeating'].fillna(False)
-df = df.drop('business_hours', axis=1)
-
-df['business_attributes.BusinessAcceptsCreditCards'] = df['business_attributes.BusinessAcceptsCreditCards'].fillna(True)
-df['business_attributes.BikeParking'] = df['business_attributes.BikeParking'].fillna(False)
-df = df.drop('business_categories', axis=1)
+df["business_attributes.DriveThru"] = df["business_attributes.DriveThru"].fillna(False)
+df["business_attributes.OutdoorSeating"] = df[
+    "business_attributes.OutdoorSeating"
+].fillna(False)
+df["business_attributes.OutdoorSeating"] = df[
+    "business_attributes.OutdoorSeating"
+].fillna(False)
+df = df.drop("business_hours", axis=1)
+df["business_attributes.BusinessAcceptsCreditCards"] = df[
+    "business_attributes.BusinessAcceptsCreditCards"
+].fillna(True)
+df["business_attributes.BikeParking"] = df["business_attributes.BikeParking"].fillna(
+    False
+)
+df = df.drop("business_categories", axis=1)
 
 # TODO
 # PriceRange --> fill with the majority value
@@ -23,11 +30,7 @@ df = df.drop('business_categories', axis=1)
 # Use Business_hours to derive hours per day and Fill empty with median time
 # Derive a decision rule to decide what a troll review is based on “helpful” votes / user attributes
 
-
 df.to_csv(OUTPUT_CSV, index=False)
 
-# Missing columns
+# Missing columns and other data
 print(df.isna().sum())
-
-
-
