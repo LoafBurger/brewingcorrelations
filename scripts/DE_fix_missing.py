896a1a197cbbc5bec0216de8ce3fac623ef588c3
Ethan <chan380@purdue.edu>
2025-10-10 12:03:26 -0400

    Data cleaning done

--- scripts/DE_fix_missing.py
+++ scripts/DE_fix_missing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime, timedelta
 
 # load the flattened CSV
 INPUT_CSV = "data/processed/starbucks_reviews_flat_all.csv"
@@ -6,14 +7,32 @@ OUTPUT_CSV = "data\processed\starbucks_reviews_cleaned.csv"
 df = pd.read_csv(INPUT_CSV)
 
 
+def hours_from_range(time_range):
+    try:
+        start_str, end_str = time_range.split("-")
+        start = datetime.strptime(start_str.strip(), "%H:%M")
+        end = datetime.strptime(end_str.strip(), "%H:%M")
+        if end < start:  # crosses midnight
+            end += timedelta(days=1)
+        diff = (end - start).seconds / 3600
+        return diff
+    except Exception:
+        return None
+
+
+# df['hours'] = df['time_range'].apply(hours_from_range)
+# print(df)
+
+
 # Fill missing values and drop irrelevant columns
 df["business_attributes.DriveThru"] = df["business_attributes.DriveThru"].fillna(False)
 df["business_attributes.OutdoorSeating"] = df[
     "business_attributes.OutdoorSeating"
 ].fillna(False)
-df["business_attributes.OutdoorSeating"] = df[
-    "business_attributes.OutdoorSeating"
-].fillna(False)
+df["business_attributes.RestaurantsTakeOut"] = df[
+    "business_attributes.RestaurantsTakeOut"
+].fillna(True)
+
 df = df.drop("business_hours", axis=1)
 df["business_attributes.BusinessAcceptsCreditCards"] = df[
     "business_attributes.BusinessAcceptsCreditCards"
@@ -21,16 +40,60 @@ df["business_attributes.BusinessAcceptsCreditCards"] = df[
 df["business_attributes.BikeParking"] = df["business_attributes.BikeParking"].fillna(
     False
 )
+
+# Fill with majority value
 df = df.drop("business_categories", axis=1)
+df["business_attributes.RestaurantsPriceRange2"] = df[
+    "business_attributes.RestaurantsPriceRange2"
+].fillna(df["business_attributes.RestaurantsPriceRange2"].value_counts().idxmax())
 
-# TODO
-# PriceRange --> fill with the majority value
-# User_friends --> covert None to ?  or add numerical amount
-# Remove rows with strings in int columns
-# Use Business_hours to derive hours per day and Fill empty with median time
-# Derive a decision rule to decide what a troll review is based on “helpful” votes / user attributes
+# Add column to count number of friends
+df["user_friend_count"] = df["user_friends"].apply(
+    lambda x: len(str(x).split(",")) if pd.notnull(x) else 0
+)
 
+
+# Derive hours for each day
+df["monday_duration"] = df["business_hours.Monday"].apply(hours_from_range)
+median_val = df["monday_duration"].median(skipna=True)
+df["monday_duration"] = df["monday_duration"].fillna(median_val)
+
+df["tuesday_duration"] = df["business_hours.Tuesday"].apply(hours_from_range)
+median_val = df["tuesday_duration"].median(skipna=True)
+df["tuesday_duration"] = df["tuesday_duration"].fillna(median_val)
+
+df["wednesday_duration"] = df["business_hours.Wednesday"].apply(hours_from_range)
+median_val = df["wednesday_duration"].median(skipna=True)
+df["wednesday_duration"] = df["wednesday_duration"].fillna(median_val)
+
+df["thursday_duration"] = df["business_hours.Thursday"].apply(hours_from_range)
+median_val = df["thursday_duration"].median(skipna=True)
+df["thursday_duration"] = df["thursday_duration"].fillna(median_val)
+
+df["friday_duration"] = df["business_hours.Friday"].apply(hours_from_range)
+median_val = df["friday_duration"].median(skipna=True)
+df["friday_duration"] = df["friday_duration"].fillna(median_val)
+
+df["saturday_duration"] = df["business_hours.Saturday"].apply(hours_from_range)
+median_val = df["saturday_duration"].median(skipna=True)
+df["saturday_duration"] = df["saturday_duration"].fillna(median_val)
+
+df["sunday_duration"] = df["business_hours.Sunday"].apply(hours_from_range)
+median_val = df["sunday_duration"].median(skipna=True)
+df["sunday_duration"] = df["sunday_duration"].fillna(median_val)
+
+# Get total hours
+df["total_hours"] = (
+    df["monday_duration"]
+    + df["tuesday_duration"]
+    + df["wednesday_duration"]
+    + df["thursday_duration"]
+    + df["friday_duration"]
+    + df["saturday_duration"]
+    + df["sunday_duration"]
+)
+
+
+print("Cleaning...")
 df.to_csv(OUTPUT_CSV, index=False)
-
-# Missing columns and other data
-print(df.isna().sum())
+print("Done!")
